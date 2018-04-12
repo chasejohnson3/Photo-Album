2fca4a51d0838c1e5b481f4c22e2a6a5a2ab7e8c
Junyan Yang <yangwu123789@gmail.com>
2018-04-12 17:36:25 -0500

    finished routes

---
+++ photo-album/src/components/IndividualComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Individual = () => {
+    return (
+        <div className="jumbotron">
+            <h1 className="display-3">Individual</h1>
+        </div>
+    );
+}
+
+export default Individual;
